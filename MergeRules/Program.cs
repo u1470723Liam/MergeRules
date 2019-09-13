17521f2cc5eb9179cf89ce9eb811df3d56273352
u1470723Liam <34135057+u1470723Liam@users.noreply.github.com>
2019-09-13 12:05:25 +0100

    aaa

--- MergeRules/Program.cs
+++ MergeRules/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,52 @@ namespace MergeRules
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter file:");
+            string file = Console.ReadLine();
+            StreamReader sr = new StreamReader(file);
+            List<string> rules = new List<string>();
+            const string fileLoc = @"C:\Users\u1470723\Documents";
+
+            string line = sr.ReadLine();
+            rules.Add(line);
+            int count = 1;
+
+            while (line != null)
+            {
+                bool add = true;
+                foreach (string rule in rules)
+                {
+                    if(sameContents(rule, line))
+                    {
+                        
+                        add = false;
+                        break;
+                    }
+                }
+                if (add)
+                {
+                    rules.Add(line);
+                }
+                line = sr.ReadLine();
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string rule in rules)
+            {
+                sb.AppendLine(rule);
+            }
+
+            Console.WriteLine(rules.Count);
+            File.WriteAllText(fileLoc + "\\mergedRules.out", sb.ToString());
+            while (true)
+            {
+
+            }
+        }
+
+        public static bool sameContents( string a, string b)
+        {
+            return String.Concat(a.OrderBy(c => c)).SequenceEqual(String.Concat(b.OrderBy(c => c)));
         }
     }
 }
